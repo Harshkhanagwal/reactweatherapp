c9f3a3fc83e9c6e80489ae371b944552cc5454bb
harshkhanagwal <harshkhanagwall29@gmail.com>
2023-02-25 15:02:35 +0530

    somechanges 2nd

--- src/App.jsx
+++ src/App.jsx
@@ -1,10 +1,16 @@
 import React from 'react'
 import Home from './pages/Home'
 import './style/index.css'
+import { BrowserRouter as Router,Routes, Route } from 'react-router-dom'
+
 const App = () => {
   return (
     <>
-        <Home/>
+       <Router>
+        <Routes>
+          <Route index element={<Home/>}/>
+        </Routes>
+      </Router>
     </>
   )
 }
